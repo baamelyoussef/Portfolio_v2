56b4800ea1cec16e6598f96e539da971f431946f
baamelyoussef <imbaamelyoussef@gmail.com>
2022-11-03 16:07:24 +0100

    changefileName

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
-import Container from '../components/Container/index'
-import Menu from '../components/Menu/index'
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import Container from "../components/Container/Container";
+import Menu from "../components/Menu/Menu";
 
 export default function Home() {
-  const [Navigate, setNavigate] = useState('about')
+  const [Navigate, setNavigate] = useState("about");
 
-  const updateNavigate=(prop)=>{
-    setNavigate(prop)
-  }
+  const updateNavigate = (prop) => {
+    setNavigate(prop);
+  };
   return (
-    <div className="container mx-auto dark:text-white" >
+    <div className="container mx-auto dark:text-white">
       {/* https://i.ibb.co/ZKgw6F1/300005468-3273608989562034-817290961671024273-n.jpg */}
-      <Menu updateNavigate={updateNavigate}/>
-      <Container navigate={Navigate}/>
-      
-</div>
-  )
+      <Menu updateNavigate={updateNavigate} />
+      <Container navigate={Navigate} />
+    </div>
+  );
 }
